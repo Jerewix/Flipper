50e297147dab6073142446d2dab1226b7c30db1c
Jerewix <85250879+Jerewix@users.noreply.github.com>
2024-03-11 12:30:03 -0300

    Add files via upload

---
+++ logoff3.ps1
@@ -0,0 +1,33 @@
+# Verifica si el archivo WindowsInput.dll ya está presente
+if (Test-Path "$env:ProgramFiles\WindowsInput.dll") {
+  # Ya está instalado, no se hace nada
+  Write-Host "WindowsInput.dll ya está instalado."
+} else {
+  # Instala el módulo WindowsInput
+  Install-Module WindowsInput -Force
+  Write-Host "WindowsInput.dll ha sido instalado."
+}
+
+# Importa el módulo WindowsInput
+Import-Module WindowsInput.dll
+
+$inputSim = New-Object WindowsInput.InputSimulator
+
+function Close-Session {
+  # Simula la pulsación de las teclas "Windows" + "L"
+  $inputSim.Keyboard.KeyPress(WindowsInput.VirtualKeyCode.VK_LCONTROL, [WindowsInput.ModifierKeys]::Windows)
+}
+
+while ($true) {
+  # Comprueba si se ha detectado alguna tecla presionada
+  if ($inputSim.Keyboard.GetKeyState(WindowsInput.VirtualKeyCode.Any)) {
+    Close-Session
+    break
+  }
+
+  # Espera 100 milisegundos
+  Start-Sleep -Milliseconds 100
+
+  # Borra el historial de PowerShell
+  Clear-History
+}
