58b4c3a866cbf8e983ca97c39eb8988db71c7fa8
Jerewix <85250879+Jerewix@users.noreply.github.com>
2024-03-11 11:18:52 -0300

    logoff2.ps1

--- logoff2.ps1
+++ logoff2.ps1
@@ -1,7 +1,3 @@
-# Elimina la línea de asignación a $dc
-# $dc='';
-
-# Instala el módulo Input si es necesario
 if (-not (Get-Module -Name Input)) {
   Install-Module Input
 }
@@ -9,30 +5,25 @@ if (-not (Get-Module -Name Input)) {
 Import-Module Input
 
 function Close-Session {
-  # Simula la pulsación de las teclas "Windows" + "L"
   [System.Windows.Input.Keyboard]::Press(
     [System.Windows.Input.KeyInterop]::ConvertToVirtualKey('L', [System.Windows.Input.ModifierKeys]::Windows)
   )
 }
 
 while ($true) {
-  # Comprueba si se ha detectado movimiento del mouse
   if ([System.Windows.Input.Mouse]::GetPosition().X -ne $lastMouseX -or
     [System.Windows.Input.Mouse]::GetPosition().Y -ne $lastMouseY) {
     Close-Session
     break
   }
 
-  # Comprueba si se ha presionado una tecla
   if ([System.Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::Any)) {
     Close-Session
     break
   }
 
-  # Almacena la posición actual del mouse
   $lastMouseX = [System.Windows.Input.Mouse]::GetPosition().X
   $lastMouseY = [System.Windows.Input.Mouse]::GetPosition().Y
 
-  # Espera 100 milisegundos
   Start-Sleep -Milliseconds 100
 }
