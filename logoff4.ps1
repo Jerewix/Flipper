7f3b85efcc43dc89823a05726f2509d30b76e00d
Jerewix <85250879+Jerewix@users.noreply.github.com>
2024-03-11 14:04:58 -0300

    Add files via upload

---
+++ logoff4.ps1
@@ -0,0 +1,47 @@
+Add-Type -TypeDefinition @'
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+public static class UserInputDetector
+{
+    [DllImport("user32.dll")]
+    public static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool LockWorkStation();
+
+    public struct LASTINPUTINFO
+    {
+        public uint cbSize;
+        public uint dwTime;
+    }
+
+    public static uint GetLastInputTime()
+    {
+        LASTINPUTINFO lastInputInfo = new LASTINPUTINFO();
+        lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
+        GetLastInputInfo(ref lastInputInfo);
+
+        return lastInputInfo.dwTime;
+    }
+}
+'@
+
+Start-Sleep -Seconds 2
+
+$lastInputTime = [UserInputDetector]::GetLastInputTime()
+
+while($true)
+{
+    Start-Sleep -Seconds 1
+
+    $currentInputTime = [UserInputDetector]::GetLastInputTime()
+    if ($currentInputTime -gt $lastInputTime)
+    {
+        [UserInputDetector]::LockWorkStation()
+        break
+    }
+
+    $lastInputTime = $currentInputTime
+}
