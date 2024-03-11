57a3da743d1d6ce47d11fb4e9a10b19f97ba31ec
Jerewix <85250879+Jerewix@users.noreply.github.com>
2024-03-11 10:51:35 -0300

    logoff.ps1

--- logoff.ps1
+++ logoff.ps1
@@ -3,35 +3,38 @@ Import-Module Input
 
 # Define la función para cerrar sesión
 function Close-Session {
-  # Simula la pulsación de las teclas "Windows" + "L"
-  [System.Windows.Input.Keyboard]::Press(
-    [System.Windows.Input.KeyInterop]::ConvertToVirtualKey('L', [System.Windows.Input.ModifierKeys]::Windows)
-  )
+ # Simula la pulsación de las teclas "Windows" + "L"
+ [System.Windows.Input.Keyboard]::Press(
+  [System.Windows.Input.KeyInterop]::ConvertToVirtualKey('L', [System.Windows.Input.ModifierKeys]::Windows)
+ )
 }
 
 # Inicia un bucle infinito que espera movimiento del mouse o pulsación de tecla
 while ($true) {
-  # Comprueba si se ha detectado movimiento del mouse
-  if ([System.Windows.Input.Mouse]::GetPosition().X -ne $lastMouseX -or
-    [System.Windows.Input.Mouse]::GetPosition().Y -ne $lastMouseY) {
-    # Cierra la sesión
-    Close-Session
-    # Sal del bucle
-    break
-  }
+ # Comprueba si se ha detectado movimiento del mouse
+ if ([System.Windows.Input.Mouse]::GetPosition().X -ne $lastMouseX -or
+  [System.Windows.Input.Mouse]::GetPosition().Y -ne $lastMouseY) {
+  # Cierra la sesión
+  Close-Session
+  # Sal del bucle
+  break
+ }
 
-  # Comprueba si se ha presionado una tecla
-  if ([System.Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::Any)) {
-    # Cierra la sesión
-    Close-Session
-    # Sal del bucle
-    break
-  }
+ # Comprueba si se ha presionado una tecla
+ if ([System.Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::Any)) {
+  # Cierra la sesión
+  Close-Session
+  # Sal del bucle
+  break
+ }
 
-  # Almacena la posición actual del mouse
-  $lastMouseX = [System.Windows.Input.Mouse]::GetPosition().X
-  $lastMouseY = [System.Windows.Input.Mouse]::GetPosition().Y
+ # Almacena la posición actual del mouse
+ $lastMouseX = [System.Windows.Input.Mouse]::GetPosition().X
+ $lastMouseY = [System.Windows.Input.Mouse]::GetPosition().Y
 
-  # Espera 100 milisegundos
-  Start-Sleep -Milliseconds 100
+ # Espera 100 milisegundos
+ Start-Sleep -Milliseconds 100
+
+ # Borra el historial de PowerShell
+ Clear-History
 }
